e9d654f9f801f66a67fad0910cc3878f0a84d3a2
Jason Forman <20210580+JRForman@users.noreply.github.com>
2018-06-27 21:58:35 -0700

    Initial layout, CSS and JS array written.

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+var wordList=[ 
+    {movie:"COMMANDO", character:"JOHN MATRIX",  coStar:"ALYSSA MILANO"},
+    {movie:"TERMINATOR", character:"TERMINATOR", coStar:"LINDA HAMILTON"},
+    {movie:"TWINS", character:"JULIUS BENEDICT", coStar:"DANNY DEVITO"},
+    {movie:"CONAN THE BARBARIAN", character:"CONAN", coStar:"JAMES EARL JONES"},
+    {movie:"TOTAL RECALL", character:"DOUGLAS QUAID", coStar:"SHARON STONE"},
+    {movie:"PREDATOR", character:"DUTCH", coStar:"JESSE VENTURA"},
+    {movie:"RUNNING MAN", character:"BEN RICHARDS", coStar:"JESSE VENTURA"},
+    {movie:"KINDERGARTEN COP", character:"KIMBLE", coStar:"LINDA HUNT"},
+    {movie:"TRUE LIES", character:"HARRY TASKER", coStar:"JAIME LEE CURTIS"},
+    {movie:"LAST ACTION HERO", character:"JACK SLATER", coStar:"CHARLES DANCE"},
+]
+
+
